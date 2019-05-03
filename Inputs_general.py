3a78f746dc5e50f7c0f252439ecf5c02f2e1cc0c
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-05-03 09:50:08 +0200

    Create Inputs_general.py

---
+++ Inputs_general.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 09:45:17 2019
+
+@author: Lisa
+"""
+
+#List of symbols 
+
+
+
+#aerodynamics
+
+
+#propulsion
+
+#structures
+
+#Performance
+
+
+#materials
+
+
+# 
