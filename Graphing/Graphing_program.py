860c86b6a677716ee6da4a1e0bf0427843308bd1
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-05-13 09:15:31 +0200

    Graphing program ready to be used, Changed name fuselage_design to initialsizing_fuselage

--- Graphing/Graphing_program.py
+++ Graphing/Graphing_program.py
@@ -11,8 +11,8 @@ import pandas as pd
 import sys
 
 # Loading data
-#path = sys.argv[1]
-path = r"D:\Bibliotheken\Documenten\OneDrive\Stijn\Documenten\TU\3th Year\DSE\DSE-23\Graphing\Example.xlsx"
+path = sys.argv[1]
+#path = r"D:\Bibliotheken\Documenten\OneDrive\Stijn\Documenten\TU\3th Year\DSE\DSE-23\Graphing\Example.xlsx"
 
 def read_excel(path):
     df = pd.read_excel(path)
@@ -32,7 +32,7 @@ title = data.axes[1][1]
 x_label = data.iloc[2,1]
 y_label = data.iloc[2,2]
 x = np.array(data.iloc[5:l,1])
-y = np.array(data.iloc[5:l,2])
+y = np.array(data.iloc[5:l,2:])
 label = data.iloc[l+1,2:]
 graph_type = data.iloc[l+2,1]
 legend = data.iloc[l+3,1]
@@ -42,11 +42,13 @@ save_path = data.iloc[l+4,1]
 fig = plt.figure()
 ax = fig.add_subplot(111)
 if graph_type == 'scatter':
-    ax.scatter(x,y, label = label)   
+    for i in range(len(y[0])):
+        ax.scatter(x,y[:,i], label = label[i])    
 if graph_type == 'plot':
-    ax.plot(x,y, label = label)    
+    for i in range(len(y[0])):
+        ax.plot(x,y[:,i], label = label[i])    
 ax.set(xlabel = x_label, ylabel = y_label, title = title)  
-ax.set(xlim = (0,max(x)*1.1), ylim = (0,max(y)*1.1))  
+ax.set(xlim = (0,np.max(x)*1.1), ylim = (0,np.max(y)*1.1))  
 ax.grid()
 if legend != 'no':
     ax.legend(loc = legend)
