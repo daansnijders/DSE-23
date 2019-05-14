0e08bd092ec20f41f479d37db0225abdbd2e4b61
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-05-14 11:23:56 +0200

    Planform implemented

--- Modules/initialsizing_planform.py
+++ Modules/initialsizing_planform.py
@@ -4,9 +4,7 @@ Created on Mon May 13 17:29:43 2019
 
 @author: Lisa
 """
-from math import *
 import numpy as np
-from inputs.constants import M_cr, M_x
 
 
 def get_S(MTOW,W_S):
@@ -28,14 +26,22 @@ def get_Cr(S,taper_ratio,b):
     return [2*S[i]/((1+taper_ratio[i])*b[i]) for i in range(3)]
 
 def get_CL(MTOW,rho,V,S):
-    return [MTOW[i]/(0.5*rho*V**2*S[i]) for i in range(3)]
+    return [MTOW[i]*9.80665/(0.5*rho*V**2*S[i]) for i in range(3)]
 
-def get_t_c(lambda_2_rad,M_x, M_cr,Cl):
-    return [(np.cos(lambda_2_rad[i])**3*(M_x-(M_cr + 0.03)*np.cos(lambda_2_rad))-0.115*Cl**1.5)/np.cos(lambda_2_rad)]
+def get_t_c(lambda_2_rad,M_x, M_cr,CL):
+    return [(np.cos(lambda_2_rad[i])**3*(M_x-(M_cr + 0.03)*np.cos(lambda_2_rad[i]))-0.115*CL[i]**1.5)/(np.cos(lambda_2_rad[i])**2) for i in range(3)]
 
+def get_MAC(Cr, taper_ratio):
+    return [Cr[i] * 2/3 * (1+taper_ratio[i] + taper_ratio[i]**2) / (1 + taper_ratio[i]) for i in range(3)]
 
+def get_Ct(Cr, taper_ratio):
+    return [Cr[i] * taper_ratio[i] for i in range(3)]
 
+def get_y_MAC(b, Cr, MAC, Ct):
+    return [b[i]/2 * (Cr[i] - MAC[i]) / (Cr[i] - Ct[i]) for i in range(3)]
 
+def get_dihedral_rad(lambda_4_rad):
+    return [3-lambda_4_rad[i]/10 + 2 for i in range(3)]
 
 
 
