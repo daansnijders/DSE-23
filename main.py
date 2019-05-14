766073d59ae1e6a0c804746e6ce665d8b4a29926
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-05-14 08:59:50 +0200

    Organising again

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 """
+Main program of DSE-23
 Created on Fri May  3 09:45:17 2019
 
 @author: Lisa
 """
-#flight parameters
 
-#List of symbols 
-
-
-
-#aerodynamics
-
-
-#propulsion
-
-#structures
-
-#Performance
-
-
-#materials
-
-
-# 
+from inputs import *
+from modules.initialsizing_undercarriage import *
