91ae0d0beaa9e3128ad8dd6e965e3ff14bcd8cac
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-05-14 09:07:13 +0200

    inputs moved to folder and splitted

---
+++ inputs/concept_3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 09:45:17 2019
+
+@author: Lisa
+"""
+#flight parameters
+
+#List of symbols 
+
+
+
+#aerodynamics
+
+
+#propulsion
+
+#structures
+
+#Performance
+
+
+#materials
+
+
+# 
