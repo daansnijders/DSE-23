806b285b3e5c2bd32c5e5c2d26daca90be9eaea3
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-05-14 10:32:17 +0200

    update2

--- inputs/constants.py
+++ inputs/constants.py
@@ -12,11 +12,11 @@ inch_to_m=0.0254
 ft_to_m=0.3048
 
 
-#flight parameters
+#FLIGHT PARAMETERS parameters
 H_ft=37000   #[feet]
 H_m=H_ft*ft_to_m      #[m]
-M_cr=0.75
-
+M_cruise=0.75
+M_x=0.935
 
 #air constants
 gamma=1.4
@@ -45,5 +45,20 @@ elif H_m<20000:
     p=p1*e**(-g/(R*T)*(H_m-11000))
     rho= rho_0*(T1/T_0)**(-g/(R*-0.0065)-1)*p/p1
     
-    
+
+
+#get cruise velocities and speed of sound
+a=(gamma*R*T)**0.5
+V_cruise=M_cruise*a
+
+#fuselage interior design 
+#data on the interior
+N_sa = 5                        # Number of seats abreast
+N_aisle = 1                     # Number of aisles
+aisle_width = 0.51              #[m]
+s_clearance = 0.02              #[m] Clearance between seat and fuselage
+seat_pitch = 32*inch_to_m       #[m]
+seat_width = 20*inch_to_m       #[m]
+armrest = 2*inch_to_m           #[m] Armrest width
+
 
