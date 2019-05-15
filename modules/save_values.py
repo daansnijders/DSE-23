986bf745925d149c113fb5c87a6cb6fe4943f1a7
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-05-15 15:20:25 +0200

    more output files

--- modules/save_values.py
+++ modules/save_values.py
@@ -6,6 +6,8 @@ Created on Tue May 14 17:20:01 2019
 """
 # Loading all variables
 from main import *
+import csv 
+
 
 # Opening file
 safety_check = input('Really want to write and start a new iteration? If so, write "yes" ')
@@ -13,13 +15,33 @@ if safety_check == 'yes':
     
     output_file = open('output.csv' ,  'w')
     
+    
     # Selecting variables to save
+    output_file.write('N_pax= ' + str(N_pax) + '\n')
+    output_file.write('R = ' + str(R) + '\n')
+    output_file.write('T_W ' + str(T_W) + '\n')
+    output_file.write('W_S ' + str(W_S) + '\n')
     output_file.write('OEW = ' + str(OEW) + '\n')
     output_file.write('MTOW = ' + str(MTOW) + '\n')
+    output_file.write('M_ff ' + str(M_ff) + '\n')
+    output_file.write('M_fuel ' + str(M_fuel) + '\n')
+    output_file.write('T_req ' + str(T_req) + '\n')
+    output_file.write('l_cabin' + str(l_cabin) + '\n')
+    output_file.write('d_f_inner' + str(d_f_inner) + '\n')
+    output_file.write('d_f_inner' + str(d_f_outer) + '\n')
+    output_file.write('A' + str(A) + '\n')
+    output_file.write('e' + str(e) + '\n')
+    output_file.write('S' + str(S) + '\n')
+    output_file.write('b' + str(b) + '\n')
+    output_file.write('x_cg' + str(x_cg) + '\n')
+    output_file.write('y_cg' + str(y_cg) + '\n')
+    output_file.write('z_cg' + str(z_cg) + '\n')
+    output_file.write('Cl_des'+str(Cl_des) + '\n')
     
     # Closing file
     output_file.close()
     print('Saved!')
     
+
 # Opening files
 exec(open("./output.csv").read())
