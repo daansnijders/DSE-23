100734766bffd5e187adf38b47b22ab62e1d4720
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-05-24 11:23:21 +0200

    bb

--- modules/class2_powerplant_defs.py
+++ modules/class2_powerplant_defs.py
@@ -8,4 +8,11 @@ Created on Fri May 24 11:20:12 2019
 def get_engine_mass():
     return 'ok'
 
-def get
+def get_airinduction_mass():
+    return 'ok'
+
+def get_fuelsystem_mass():
+    return 'ok'
+
+def get_propulsionsystem_mass():
+    return 'ok'
--- modules/class2_struct_defs.py
+++ modules/class2_struct_defs.py
@@ -7,4 +7,23 @@ Created on Fri May 24 09:30:42 2019
 
 
 def get_wing_mass(MTOW):
-    return MTOW/5
+    return 'ok'
+
+def get_fuselage_mass():
+    
+    return 'ok'
+
+
+def get_empennage_mass():
+    return 'ok'
+
+def get_nacelle_mass():
+    return 'ok'
+
+def get_landinggear_mass():
+    return 'ok'
+
+def get_engine_mass():
+    return 'ok'
+
+def get
