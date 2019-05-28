777adc90c4698aafb4f8b6d87be32e9842a7feb9
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-05-28 09:40:47 +0200

    sum

--- modules/class2.py
+++ modules/class2.py
@@ -83,8 +83,8 @@ class Class2_weight:
         M_paint      = get_paint_mass(self.M_TO)
         
         M_fixedequipment= get_fixedequipment_mass(M_fc,M_hydr,M_els,M_avion,M_environ,M_oxygen,M_apu,M_furnish,M_cargohand,M_operation,M_flighttest,M_paint)
-        return M_fixedequipment* lbs_to_kg
-        #return M_fc, M_hydr, M_els, M_avion, M_environ, M_oxygen, M_apu, M_cargohand, M_operation, M_flighttest, M_paint
+        #return  M_fixedequipment* lbs_to_kg
+        return M_fc* lbs_to_kg, M_hydr* lbs_to_kg, M_els* lbs_to_kg, M_avion* lbs_to_kg, M_environ* lbs_to_kg, M_oxygen* lbs_to_kg, M_apu* lbs_to_kg, M_cargohand* lbs_to_kg, M_operation* lbs_to_kg, M_flighttest* lbs_to_kg, M_paint* lbs_to_kg,M_fixedequipment* lbs_to_kg
 
    
     def OEW(self,M_structure,M_powerplant,M_fixedequipment):
@@ -116,7 +116,7 @@ config1.class2=config1.OEW(struct_1,power_1,fixedeq_1)
 config2.class2=config2.OEW(struct_2,power_2,fixedeq_2)
 config3.class2=config3.OEW(struct_3,power_3,fixedeq_3)
 
-print(struct_1,power_1,fixedeq_1,'oew',config1.class2)
+print(fixedeq_1,)
 
-print(struct_2,power_2,fixedeq_2,'oew',config2.class2)
-print(struct_3,power_3,fixedeq_3,'oew',config3.class2)
+print(fixedeq_2)
+print(fixedeq_3)
