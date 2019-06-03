07cdacbb2f0169aa7c38d49dea866974f49593f6
Anique9 <48210732+Anique9@users.noreply.github.com>
2019-06-03 15:49:50 +0200

    Update

--- modules/Aerodynamics.py
+++ modules/Aerodynamics.py
@@ -9,8 +9,22 @@ from math import *
 import numpy as np
 
 class HLD:
-    def __HLD__(self, Cl_TO, CLmax, S, A, lambda_4_rad, taper_ratio):
-        Delta_CLmax = Cl_TO - CLmax
-        lambda_hl_rad = np.arctan(np.tan(lambda_4_rad)-4/A*(13/20-1/4)(1-taper_ratio)/(1+taper_ratio))
+    def __init__(self, Cl_TO, CLmax, S, A, lambda_4_rad, taper_ratio):
+        self.Cl_TO          = Cl_TO
+        self.CLmax          = CLmax
+        self.S              = S
+        self.A              = A
+        self.lambda_4_rad   = lambda_4_rad
+        self.taper_ratio    = taper_ratio
         
+    
+    def __HLD__(self):
+        self.Delta_CLmax = Cl_TO - CLmax
+        self.hl = 13/20              # Location hinge line on chord
+        self.lambda_hl_rad = np.arctan(np.tan(lambda_4_rad)-4/A*(hl-1/4)(1-taper_ratio)/(1+taper_ratio))
+        self.c_prime = 1 + 0.57*(1-hl)
+        self.Delta_cl = 1.3*(c_prime)
         
+        self.SWF = (Delta_CLmax*S)/(0.9*Delta_cl*np.cos(lambda_hl_rad))
+        
+        return(SWF)
