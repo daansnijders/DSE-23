244a6179da8031099baf01879b77b73b3774cadc
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-06-03 12:48:56 +0200

    class 2

--- modules/class2.py
+++ modules/class2.py
@@ -15,10 +15,9 @@ from inputs.performance_inputs import *
 loadfactor=[2.8,2.8,2.8]
 V_dive=[300,300,300]
 class Class2_weight:
-    def __init__(self,config, N_pax, MTOW,M_carried_canard,M_MZF, loadfactor,V_dive, M_fuel ,T_req,l_f,d_f_inner,d_f_outer,l_cabin,l_h,S,S_c, b,b_c, S_v,S_h,Cr_t,Cr_t_c,lambda_2_rad,lambda_h_2_rad, lambda_v_2_rad,lambda_c_2_rad, S_fus):
+    def __init__(self,config, N_pax, MTOW,M_carried_canard_MZF,M_MZF, loadfactor,V_dive, M_fuel ,T_req,l_f,d_f_inner,d_f_outer,l_cabin,l_h,S,S_c, b,b_c, S_v,S_h,Cr_t,Cr_t_c,lambda_2_rad,lambda_h_2_rad, lambda_v_2_rad,lambda_c_2_rad, S_fus):
         self.config         = config
-        self.M_TO           = MTOW                                              # [kg]
-        self.M_carried_canard=M_carried_canard                                  # [KG]    
+        self.M_TO           = MTOW                                              # [kg] 
         self.n_ult          = 1.5*loadfactor                                    # [-]
         self.V_dive         = V_dive                                            # []?
         self.M_fuel         = M_fuel                                            # []?
@@ -45,6 +44,7 @@ class Class2_weight:
         self.w_fus          = self.d_f_outer/2                                  # [m]
         self.h_fus          = self.d_f_outer/2                                  # [m]
         self.M_MZF          = M_MZF                                             # [kg]?
+        self.M_carried_canard_MZF= M_carried_canard_MZF
         
     def structural_mass(self):
         M_wing          =get_wing_mass(self.M_MZF,self.b,self.S,self.Cr_t,self.lambda_2_rad,self.n_ult)*0.95
@@ -53,7 +53,7 @@ class Class2_weight:
         if self.config==1:
             M_canard    =0
         else:
-            M_canard         =get_wing_mass(self.M_MZF,self.b_c,self.S_c,self.Cr_t_c,self.lambda_c_2_rad,self.n_ult)
+            M_canard         =get_wing_mass(self.M_carried_canard_MZF,self.b_c,self.S_c,self.Cr_t_c,self.lambda_c_2_rad,self.n_ult)
         
         M_horizontaltail   =get_horizontaltail_mass(K_h,self.S_h,self.V_dive,self.lambda_h_2_rad)
         M_verticaltail     =get_verticaltail_mass(K_v,self.S_v,self.V_dive,self.lambda_v_2_rad)
