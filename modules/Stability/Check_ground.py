ef56bfd73215b3eb4da8e5ccf4e3d643dc2faca2
Daan Snijders <47558924+daansnijders@users.noreply.github.com>
2019-06-04 11:08:38 +0200

    update

--- modules/Stability/Check_ground.py
+++ modules/Stability/Check_ground.py
@@ -4,6 +4,18 @@ Created on Tue Jun  4 09:17:48 2019
 
 @author: daansnijders
 """
+from modules.loaddiagram_detailed import *
+from modules.Stability.Stability_runfile import *
 
 class Stability_check_ground:
-    def __init__ (self, )
+    def __init__ (self, cg_min, cg_max, weight_max):
+        self.cg_min=cg_min
+        self.cg_max=cg_max
+        self.weight_max=weight_max
+        
+               
+    def check_equilibrium(self):
+        
+        
+        
+        return check
