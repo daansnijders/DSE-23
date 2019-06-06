aaa4ad837e120c3597e8c34bb9b55c1ebf7d4685
Daan Snijders <47558924+daansnijders@users.noreply.github.com>
2019-06-06 11:27:53 +0200

    Update

--- modules/Stability/Stability_runfile_empennage.py
+++ modules/Stability/Stability_runfile_empennage.py
@@ -15,6 +15,7 @@ from modules.EXECUTE_FILE import *
 
 x_cg_config1_range = [config1_cg_x - 0.1* l_f[0],config1_cg_x,config1_cg_x + 0.1* l_f[0]]
 x_cg_wing_config1_range = [config1_cg.x_cg_wing - 0.1* l_f[0], config1_cg.x_cg_wing, config1_cg.x_cg_wing + 0.1* l_f[0]]
+x_lemac_range = [12.13, 13., 14.]
 
 config1_load      = Loading_diagram(x_cargo[0], l_f[0], l_cabin[0], seat_pitch, N_pax[0], N_sa, OEW[0], MTOW[0], x_cg_config1_range[0], MAC, S, b, A, Xfirst, M_payload[0], M_cargo_available[0], M_fuel[0], M_pax, M_carry_on, x_cg_wing_config1_range[0], -1)     
 config1_load2      = Loading_diagram(x_cargo[0], l_f[0], l_cabin[0], seat_pitch, N_pax[0], N_sa, OEW[0], MTOW[0], x_cg_config1_range[1], MAC, S, b, A, Xfirst, M_payload[0], M_cargo_available[0], M_fuel[0], M_pax, M_carry_on, x_cg_wing_config1_range[1], 0)     
@@ -49,9 +50,16 @@ frac_min[0], frac_max[0], frac1 = config1_ground.check_equilibrium()
 frac_min[1], frac_max[1], frac2 = config1_ground2.check_equilibrium()
 frac_min[2], frac_max[2], frac2 = config1_ground3.check_equilibrium()
 
+x_cg_max1 = [0, 0, 0]
+x_cg_min1 = [0, 0, 0]
 
-x_cg_max = max(xcg_max[0], xcg_max[3]) + 0.05*MAC
-x_cg_min = min(xcg_min[0], xcg_min[3]) - 0.05*MAC
+x_cg_max1[0] = max(xcg_max[0], xcg_max[3]) + 0.05*MAC
+x_cg_max1[1] = max(xcg_max[1], xcg_max[4]) + 0.05*MAC
+x_cg_max1[2] = max(xcg_max[2], xcg_max[5]) + 0.05*MAC
+x_cg_min1[0] = min(xcg_min[0], xcg_min[3]) - 0.05*MAC
+x_cg_min1[1] = min(xcg_min[1], xcg_min[4]) - 0.05*MAC
+x_cg_min1[2] = min(xcg_min[2], xcg_min[5]) - 0.05*MAC
+
 #print ("The most aft CG position from the nose for configuration 1 during flight is: ", max(xcg_max[0], xcg_max[3]))
 #print("Including a 0,05 m stability margin we get", max(xcg_max)+0.05)
 #
