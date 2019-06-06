d73acea56e3508e40b29f05c007d663a150d93a4
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-06-06 17:36:48 +0200

    noise

--- modules/sustainability/noise_calc.py
+++ modules/sustainability/noise_calc.py
@@ -5,37 +5,58 @@ Created on Wed Jun  5 15:46:51 2019
 @author: Lisa
 """
 import numpy as np
-from inputs.concept_1 import b, S
-from inputs.constants import *
-flap_deflection_rad=radians(40)
+from inputs.concept_1 import b, S, V_TO, N_mw, N_nw, z_mlg
+from inputs.constants import mu_sl, rho_0,M_cruise
+
+d_mw=1.5 #[m]
+d_nw=0.7    #[m]
+flap_deflection_rad=np.radians(40)
+L_strut= -z_mlg
+diameter_strut = 0.3
+
 #noise constants from ANOPP method
-#G_wing=0.37*S/b**2*((rho*M_cruise*a*S)/(mu_0*b))**-0.2
-#L_wing=G_wing*b
-#K_wing=4.464E-5 
-#a_wing=5
-#
-#G_slat=G_wing
-#L_slat=L_wing
-#K_slat=K_wing
-#a_slat=a_wing
-#
-#G_flap=S_flap/b**2*(sin(flap_deflection_rad))**2
-#L_flap=S_flap/b_flap
-#K_flap=2.787*10**-4
-#a_flap=6
-#
-#G_lg=N_mw*(d_mw/b)**2
-#l_lg=d_mw
-#K_lg=3.414E-4  #4 wheels
-#K_nlg=4.349E-4 #1 or 2 wheels (nose)
-#a_lg=6
-#
-#K_strut=2.753E-4 
-##radius definitions
-#r_lateral=450
-#r_app=2000
-#r_flyover=6500
+
+G_wing=0.37*S/b**2*((rho_0*M_cruise*a*S)/(mu_sl*b))**-0.2
+L_wing=G_wing*b
+K_wing=4.464E-5 
+a_wing=5
+
+G_slat=G_wing
+L_slat=L_wing
+K_slat=K_wing
+a_slat=a_wing
+
+flap_deflection_rad=radians(40)
+b_flap=b/2*0.6
+c_flap=0.35*Cr
+S_flap=b_flap*c_flap
+
+G_flap=S_flap/b**2*(sin(flap_deflection_rad))**2
+L_flap=S_flap/b_flap
+K_flap=2.787*10**-4
+a_flap=6
+
+#G_lg_m=N_mw*(d_mw/b)**2
+#G_lg_n=N_nw*(d_nw/b)**2
+
+K_lg=3.414E-4  #4 wheels
+K_nlg=4.349E-4 #1 or 2 wheels (nose)
+K_strut=2.753E-4 
+
+
+
+'CONDITIONS WHERE WE EVALUATE THE NOISE'
+#radius definitions
+r_lateral=450
+r_app=2000
+r_flyover=6500
     
+#get r and phi and theta for the observers
+
+
+
+
+
 #start with the  1/3 octave bands 
 number_of_bands=43
 bandnumbers=list(range(1,number_of_bands+1))
--- modules/sustainability/noise_defs.py
+++ modules/sustainability/noise_defs.py
@@ -7,19 +7,29 @@ Created on Tue Jun  4 11:53:00 2019
 from math import *
 #noise definitions
 
+
+'set up the effective pressure of the different airframe components'
 #f will be a list which will be an 1./third octave band or complete octave band 
-def get_strouhal_number(f,L,M,theta,a):
+
+'strouhal numbers'
+def get_strouhal_number(L,M,theta,a):
     S=f*L*(1-M*cos(theta))/(M*a)
     return S
 
-def get_strouhal_number_lg_and_strut(f,d,V):
-    S=f*d/V # normal Mach times speed of sound which is V uses both the 
+def get_strouhal_number_lg_and_strut(d,M,a):
+    S=f*d/(M*a) # 
     return S
-
-def get_power(K,M,a_const,G,rho,a,b):
+'get acoustic powers'
+def get_noise_acoustic_power(K,M,a_const,G,rho,a,b):
     P=K*M**a_const*G*(rho*a**3*b**2)
     return P
-    
+def get_noise_acoustic_power_landinggear(K,M,N_w,d_w):
+    return K*M**6*N_w*d_w**2
+
+def get_noise_acoustic_power_strut(K,M,d_strut,L_strut):
+    return K*M**6*d_strut*L_strut
+
+'spectrical functions'
 def get_spectrical_function_wing(S):
     F=0.613*(10*S)**4*((10*S)**1.5+0.5)**-4
     return F
@@ -40,6 +50,7 @@ def get_spectrical_function_landinggear(S): #this is for two wheels
     F=2*(13.59*S**2*(S**2+12.5)**-2.25)
     return F
 
+'get directivity functions'
 def get_directivity_function_wing(theta,phi):  #same for slat
     D=4*(cos(phi))**2*(cos(theta/2))**2
     return D
@@ -52,19 +63,68 @@ def get_directivity_function_landinggear(theta):
     D=3/2*(sin(theta))**2
     return D
 
-def get_effective_pressure(rho,a,P,D,F,r_observer,M,theta):
+def get_directivity_function_strut(theta,phi):
+    D=3*(sin(theta))**2*(sin(phi))**2
+    return D
+'this is to get the effective pressure level (squared)'
+
+def get_effective_pressure_flap(rho,a,M,r_observer,theta,phi,L,K,a_const,G,flap_deflection):
+    Strouhal=get_strouhal_number(L,M,theta,a)
+    F=get_spectrical_function_flap(Strouhal)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a,b)
+    D=get_directivity_function_flap(flap_deflection_rad,theta,phi)
+    
     p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
     return p_e_squared
 
+def get_effective_pressure_slat(rho,a,M,r_observer,theta,phi,L,K,a_const,G):
+    Strouhal=get_strouhal_number(L,M,theta,a)
+    F=get_spectrical_function_slat(Strouhal)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a,b)
+    D=get_directivity_function_wing(theta,phi)
+    
+    p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
+    return p_e_squared
+
+def get_effective_pressure_wing(rho,a,M,r_observer,theta,phi,L,K,a_const,G):
+    Strouhal=get_strouhal_number(L,M,theta,a)
+    F=get_spectrical_function_wing(Strouhal)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a,b)
+    D=get_directivity_function_wing(theta,phi)
+    
+    p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
+    return p_e_squared
+
+def get_effective_pressure_lg(rho,a,M,r_observer,theta,phi,d,K,N_w,d_w):
+    Strouhal=get_strouhal_number_lg_and_strut(d,M,a)
+    F=get_spectrical_function_landinggear(Strouhal)
+    P=get_noise_acoustic_power_landinggear(K,M,N_w,d_w)
+    D=get_directivity_function_landinggear(theta)
+    
+    p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
+    return pe_e_squared
+
+def get_effective_pressure_strut(rho,a,M,r_observer,theta,phi,d,K,d_strut,L_strut):
+    Strouhal=get_strouhal_number_lg_and_strut(d,M,a)
+    F=get_spectrical_function_landinggear(Strouhal)
+    P=get_noise_acoustic_power_strut(K,M,d_strut,L_strut)
+    D=get_directivity_function_strut(theta,phi)
+    
+    p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
+    return pe_e_squared
+
+
+
+'get the sound pressure level'
 def get_sound_pressure_level(p_e_squared):   #[dB]
     SPL=10*log10(p_e_squared/(2E-5)**2)
     return SPL
-
+'correct for the third octave bands'
 def correction_for_thirdbandwidth(SPL,freq_delta):
     SPL_bandwidth=SPL+10*log10(freq_delta)
     return SPL_bandwidth
 
-    
+'perform A weightening '  
 def a_weighting_correction(f):
     delta_L_A=-145.528+98.262*log10(f)-19.509*(log10(f))**2+0.975*(log(f))**3
     return delta_L_A
@@ -74,6 +134,8 @@ def A_weighted_sound_level(delta_L_A,SPL):
     OSPL_A=10*log10(sum(10**(L_A_perfreq/10)))
     return OSPL_A
 
+
+'redundant definitions for now '
 def get_overall_sound_pressure_level(SPL,centrefreq):
     OSPL=10*log10(sum(10**(SPL/10)))
     return OSPL
@@ -84,7 +146,13 @@ def get_perceived_noise_level(N):
 
 def get_effective_perceived_noise_level(L_pn,delta_t):
     L_epn=10*log10(delta_t/10*sum(10**(L_pn/10)))
-   
+       
     
     
     
+
+def get_theta_observer(x,R):
+    return 'ok'
+
+def get_phi_observer(x,R):
+    return 'ok'
