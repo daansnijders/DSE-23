e58f1f2378fa58cc287be6dbb93bcccdb37924dd
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-06-06 10:49:39 +0200

    Added cg range for stabitilty

--- modules/Stability/Stability_runfile.py
+++ modules/Stability/Stability_runfile.py
@@ -11,6 +11,10 @@ from inputs.constants import *
 
 from modules.loaddiagram_detailed import *
 from modules.Stability.Check_ground import *
+from modules.EXECUTE_FILE import *
+
+x_cg_config1_range = [config1_cg_x - 0.1* l_f[0],config1_cg_x,config1_cg_x + 0.1* l_f[0]]
+x_cg_wing_config1_range = [config1_cg.x_cg_wing - 0.1* l_f[0], config1_cg.x_cg_wing, config1_cg.x_cg_wing + 0.1* l_f[0]]
 
 
 config1_load      = Loading_diagram(x_cargo[0], l_f[0], l_cabin[0], seat_pitch, N_pax[0], N_sa, OEW[0], MTOW[0], x_cg[0], y_cg[0], z_cg, MAC, S, b, A, Xfirst, M_payload[0], M_cargo_available[0], M_fuel[0], M_pax, M_carry_on, x_cg_wing[0], 1)     
