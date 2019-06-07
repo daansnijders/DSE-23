27b4cdd0d77e4c832b401e97f0bd8985ea89beab
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-06-07 09:00:22 +0200

    Some format changes

--- modules/Stability/empennage.py
+++ modules/Stability/empennage.py
@@ -4,101 +4,14 @@ Created on Wed Jun  5 09:50:12 2019
 
 @author: Stijn
 """
+import numpy as np
+import matplotlib.pyplot as plt
 from inputs.constants import *
 from inputs.concept_1 import *
 from modules.Stability.Stability_runfile_empennage import x_cg_min1, x_cg_max1, x_le_MAC_range_perc, x_le_MAC_range
 from modules.Stability.Stability_runfile import x_cg_min, x_cg_max
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-alpha0 = np.deg2rad(-5)
-C_m_ac_w = -0.3
-C_L_w_a = 2*np.pi
-C_D_w = 0.05
-
-class empennage(object):
-    def __init__(self,config,C_L_w,C_D_w,C_M_ac_w , aoa_rad,x_cg):
-        self.N_engine = T_req[config] * np.sin(i_e_rad)
-        self.T_engine = T_req[config] * np.cos(i_e_rad)
-#        self.C_M_ac_w = C_M_ac_w
-#        self.C_N_w = C_L_w * np.cos(aoa_rad) + C_D_w * np.sin(aoa_rad)
-#        self.C_T_w = C_D_w * np.cos(aoa_rad) - C_L_w * np.sin(aoa_rad)
-        self.config = config
-        self.x_w = x_le_MAC + 0.25*MAC # 0.25?
-        self.z_w = t_c * MAC /2
-        self.x_cg = x_cg
-        
-    def calc_C_N(self): 
-        self.C_N = MTOW[self.config]/(0.5*rho*V_cruise**2 * S)*np.cos(theta_rad)
-        return self.C_N
-    
-    def calc_x_h(self):
-        if self.config == 1:
-            A = (self.calc_C_N() - self.C_N_w - self.N_engine/(0.5*rho*V_cruise**2*S)*np.cos(i_e_rad) - self.T_engine/(0.5*rho*V_cruise**2*S)*np.sin(i_e_rad))
-            
-            x_h = self.x_cg + self.C_M_ac_w *MAC/A + self.C_N_w * (self.x_cg - self.x_w)/A - self.C_T_w * (z_cg - self.z_w) + (self.T_engine * np.cos(i_e_rad) - self.N_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (z_cg - z_engine) + (self.N_engine * np.cos(i_e_rad) + self.T_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (self.x_cg - x_engine)
-        return x_h
-    
-    def findeq(self):
-        aoa_rad = np.linspace(np.deg2rad(0),np.deg2rad(5),1000)
-        y1 = MTOW[self.config]*g / (0.5*rho*V_cruise**2*S) * np.cos(aoa_rad) - C_L_w_a * (aoa_rad - alpha0) * np.cos(aoa_rad) - C_D_w * np.sin(aoa_rad) - self.N_engine / (0.5*rho*V_cruise**2*S) * np.cos(i_e_rad) - self.T_engine/(0.5*rho*V_cruise**2*S)*np.sin(i_e_rad)
-        y2 = (-C_m_ac_w - (C_L_w_a * (aoa_rad - alpha0) * np.cos(aoa_rad) - C_D_w * np.sin(aoa_rad)) * (x_cg[self.config] - self.x_w[self.config])/MAC + (C_D_w * np.cos(aoa_rad) - C_L_w_a * (aoa_rad - alpha0) * np.sin(aoa_rad)) * (z_cg - self.z_w) / MAC - (self.T_engine * np.cos(i_e_rad) - self.N_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (z_cg - z_engine) + (self.N_engine * np.cos(i_e_rad) + self.T_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (self.x_cg - x_engine[self.config])) * MAC / (self.x_cg - x_le_h[self.config])
-    
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.plot(np.rad2deg(aoa_rad),y1)
-        ax.plot(np.rad2deg(aoa_rad),y2)
-        
-        start = y1[0] < y2[0]
-        condition = start
-        i = 1
-        while condition == start:
-            condition = y1[i] < y2[i]
-            i += 1
-        
-        aoa_cruise = aoa_rad[i]
-        y_cruise = np.sum([y1[i],y2[i]])/2
-        
-        
-        return np.rad2deg(aoa_cruise),y_cruise
-        
-#e = empennage(1,1.0,0.02,0,np.deg2rad(1.0),x_cg_min)
-#e.findeq()
-
-"""
-Values needed
-M = 0
-M_ac_w = sybren
-M_ac_h = 0
-*M_ac_c
-C_N_w = sybren
-C_N_h = sybren
-*N_c 
-T_w = tbc wrt N_w
-T_h = tbc wrt N_h
-*T_c
-x_cg = get_cg
-z_cg = get_cg
-*x_w
-z_w = around 0 + 0.5 * t
-x_h = 0.9 of l_f?
-z_h = 0.8 * d_f_outer
-*x_c
-*z_c
-N_e = np.sin(i_e) * thrust
-T_e = np.cos(i_e) * thrust
-i_e = 0
-x_e = get_cg
-z_e = get_cg
-V_cruise = concept 1
-V_h = ?
-V_c = ?
-theta/aoa = aerodynamic guys -> alpha cruise
-we need cl/alpha in order to determine the lift produced.
-"""
-
-class empennage2:
+class empennage:
     def __init__(self, config, x_ac, CL_a_h, CL_a_ah, de_da, S_h, l_h, S, c, V_h, V, x_le_MAC, Cm_ac, CL_ah, x_cg, CL_h):   
         self.config = config
         self.x_ac=x_ac #from nose in [m]
@@ -199,42 +112,68 @@ class empennage2:
         Cr_h = get_Cr_h(S_h, taper_ratio_h, b_h)                                    # [m] root chord length horizontal tail
         Ct_h = get_Ct_h(Cr_h, taper_ratio_h)                                        # [m] tip chord length horizontal tail
         
+        
+
+        # =============================================================================
+        # Vertical tail
+        # =============================================================================
+                
+        V_h = 1.28                                                              # [-] volume horizontal tail
+        A_h = 4.95                                                              # [-] aspect ratio horizontal tail
+        taper_ratio_h = 0.39                                                    # [-] taper ratio horizontal tail
+        V_v = 0.1                                                               # [-] volume vertical tail
+        A_v = 1.9                                                               # [-] aspect ratio vertical tail
+        taper_ratio_v = 0.375                                                   # [-] taper ratio vertical tail
+        lambda_v_le_rad = np.deg2rad(40)                                        # [rad] leading edge sweep angle vertical tail
+        
+        def get_S_v(S, b, x_cg, V_v, x_v):
+            return [V_v*S* b / (x_v[i] - x_cg[i]) for i in range(3)]
+         
+        def get_b_v(S_v, A_v):
+            return [np.sqrt(S_v*A_v) for i in range(3)]
+        
+        def get_Cr_v(S_v, taper_ratio_v, b_v):
+            return [2*S_v/((1+taper_ratio_v)*b_v[i]) for i in range(3)]
+        
+        def get_Ct_v(Cr_v, taper_ratio_v):
+            return [Cr_v[i] * taper_ratio_v for i in range(3)]
+    
+        S_v = max(get_S_v(S, b, x_cg, V_v, x_le_v))                             # [m^2] surface area vertical tail
+        b_v = get_b_v(S_v, A_v)                                                 # [m] span vertical tail
+        Cr_v = get_Cr_v(S_v, taper_ratio_v, b_v)                                # [m] root chord lengh vertical tail
+        Ct_v = get_Ct_v(Cr_v, taper_ratio_v)                                    # [m] tip chord length vertical tail
+
+        lambda_v_4_rad = get_lambda_4_rad_from_lambda_le(lambda_v_le_rad,Cr_v,b_v,taper_ratio_v)
+        lambda_v_2_rad = get_lambda_2_rad(lambda_v_4_rad,A_v,taper_ratio_v)
+        
         return self.Sh_S1, self.Sh_S2, self.Sh_C1, Sh_S, x_le_MAC, S_h
-
-    # =============================================================================
-    # Vertical tail
-    # =============================================================================
-            
-    V_h = 1.28                                                                  # [-] volume horizontal tail
-    A_h = 4.95                                                                  # [-] aspect ratio horizontal tail
-    taper_ratio_h = 0.39                                                        # [-] taper ratio horizontal tail
-    V_v = 0.1                                                                   # [-] volume vertical tail
-    A_v = 1.9                                                                   # [-] aspect ratio vertical tail
-    taper_ratio_v = 0.375                                                       # [-] taper ratio vertical tail
     
-    def get_S_v(S, b, x_cg, V_v, x_v):
-        return [V_v*S* b / (x_v[i] - x_cg[i]) for i in range(3)]
-     
-    def get_b_v(S_v, A_v):
-        return [np.sqrt(S_v*A_v) for i in range(3)]
-    
-    def get_Cr_v(S_v, taper_ratio_v, b_v):
-        return [2*S_v/((1+taper_ratio_v)*b_v[i]) for i in range(3)]
-    
-    def get_Ct_v(Cr_v, taper_ratio_v):
-        return [Cr_v[i] * taper_ratio_v for i in range(3)]
-    
-
-    S_v = max(get_S_v(S, b, x_cg, V_v, x_le_v))                                 # [m^2] surface area vertical tail
-    b_v = get_b_v(S_v, A_v)                                                     # [m] span vertical tail
-    Cr_v = get_Cr_v(S_v, taper_ratio_v, b_v)                                    # [m] root chord lengh vertical tail
-    Ct_v = get_Ct_v(Cr_v, taper_ratio_v)                                        # [m] tip chord length vertical tail
-    
-e2 = empennage2(1, (11.78+0.25*3.8), 3.82, 4.90, 0.3835, 21.72, 16., 93.5, 3.8, 1., 1., 11.78, -0.3, 1.6, x_cg_max, -0.5838, )
+e2 = empennage(1, (11.78+0.25*3.8), 3.82, 4.90, 0.3835, 21.72, 16., 93.5, 3.8, 1., 1., 11.78, -0.3, 1.6, x_cg_max, -0.5838, )
 
 r = e2.plot_stability_horitail()   
-#q = e2.plot_stability()
+
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #N_e = thrust_max/2 * y_engine                                                   # [N*m] moment caused by engine inoperative
 #
@@ -243,3 +182,86 @@ r = e2.plot_stability_horitail()
 #N_v = - Y_v * l_v                                                               # [N*m] moment caused by the vertical tail
 
 
+#alpha0 = np.deg2rad(-5)
+#C_m_ac_w = -0.3
+#C_L_w_a = 2*np.pi
+#C_D_w = 0.05
+#
+#class empennage(object):
+#    def __init__(self,config,C_L_w,C_D_w,C_M_ac_w , aoa_rad,x_cg):
+#        self.N_engine = T_req[config] * np.sin(i_e_rad)
+#        self.T_engine = T_req[config] * np.cos(i_e_rad)
+##        self.C_M_ac_w = C_M_ac_w
+##        self.C_N_w = C_L_w * np.cos(aoa_rad) + C_D_w * np.sin(aoa_rad)
+##        self.C_T_w = C_D_w * np.cos(aoa_rad) - C_L_w * np.sin(aoa_rad)
+#        self.config = config
+#        self.x_w = x_le_MAC + 0.25*MAC # 0.25?
+#        self.z_w = t_c * MAC /2
+#        self.x_cg = x_cg
+#        
+#    def calc_C_N(self): 
+#        self.C_N = MTOW[self.config]/(0.5*rho*V_cruise**2 * S)*np.cos(theta_rad)
+#        return self.C_N
+#    
+#    def calc_x_h(self):
+#        if self.config == 1:
+#            A = (self.calc_C_N() - self.C_N_w - self.N_engine/(0.5*rho*V_cruise**2*S)*np.cos(i_e_rad) - self.T_engine/(0.5*rho*V_cruise**2*S)*np.sin(i_e_rad))
+#            
+#            x_h = self.x_cg + self.C_M_ac_w *MAC/A + self.C_N_w * (self.x_cg - self.x_w)/A - self.C_T_w * (z_cg - self.z_w) + (self.T_engine * np.cos(i_e_rad) - self.N_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (z_cg - z_engine) + (self.N_engine * np.cos(i_e_rad) + self.T_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (self.x_cg - x_engine)
+#        return x_h
+#    
+#    def findeq(self):
+#        aoa_rad = np.linspace(np.deg2rad(0),np.deg2rad(5),1000)
+#        y1 = MTOW[self.config]*g / (0.5*rho*V_cruise**2*S) * np.cos(aoa_rad) - C_L_w_a * (aoa_rad - alpha0) * np.cos(aoa_rad) - C_D_w * np.sin(aoa_rad) - self.N_engine / (0.5*rho*V_cruise**2*S) * np.cos(i_e_rad) - self.T_engine/(0.5*rho*V_cruise**2*S)*np.sin(i_e_rad)
+#        y2 = (-C_m_ac_w - (C_L_w_a * (aoa_rad - alpha0) * np.cos(aoa_rad) - C_D_w * np.sin(aoa_rad)) * (x_cg[self.config] - self.x_w[self.config])/MAC + (C_D_w * np.cos(aoa_rad) - C_L_w_a * (aoa_rad - alpha0) * np.sin(aoa_rad)) * (z_cg - self.z_w) / MAC - (self.T_engine * np.cos(i_e_rad) - self.N_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (z_cg - z_engine) + (self.N_engine * np.cos(i_e_rad) + self.T_engine * np.sin(i_e_rad)) / (0.5*rho*V_cruise**2*S*MAC) * (self.x_cg - x_engine[self.config])) * MAC / (self.x_cg - x_le_h[self.config])
+#    
+#        fig = plt.figure()
+#        ax = fig.add_subplot(111)
+#        ax.plot(np.rad2deg(aoa_rad),y1)
+#        ax.plot(np.rad2deg(aoa_rad),y2)
+#        
+#        start = y1[0] < y2[0]
+#        condition = start
+#        i = 1
+#        while condition == start:
+#            condition = y1[i] < y2[i]
+#            i += 1
+#        
+#        aoa_cruise = aoa_rad[i]
+#        y_cruise = np.sum([y1[i],y2[i]])/2
+#        
+#        
+#        return np.rad2deg(aoa_cruise),y_cruise
+
+
+"""
+Values needed
+M = 0
+M_ac_w = sybren
+M_ac_h = 0
+*M_ac_c
+C_N_w = sybren
+C_N_h = sybren
+*N_c 
+T_w = tbc wrt N_w
+T_h = tbc wrt N_h
+*T_c
+x_cg = get_cg
+z_cg = get_cg
+*x_w
+z_w = around 0 + 0.5 * t
+x_h = 0.9 of l_f?
+z_h = 0.8 * d_f_outer
+*x_c
+*z_c
+N_e = np.sin(i_e) * thrust
+T_e = np.cos(i_e) * thrust
+i_e = 0
+x_e = get_cg
+z_e = get_cg
+V_cruise = concept 1
+V_h = ?
+V_c = ?
+theta/aoa = aerodynamic guys -> alpha cruise
+we need cl/alpha in order to determine the lift produced.
+"""
