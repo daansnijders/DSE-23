6cb5191473f0e2393635b201aad4fbcb262d1d9d
Lisakestens <37894272+Lisakestens@users.noreply.github.com>
2019-06-07 17:25:48 +0200

    noise

--- modules/sustainability/execute_noise.py
+++ modules/sustainability/execute_noise.py
@@ -7,14 +7,63 @@ Created on Thu Jun  6 16:41:25 2019
 from modules.sustainability.noise_defs import *
 from modules.sustainability.noise_calc import *
 
-pe_2_flap =get_effective_pressure_flap(rho_0,a_sl,M,r_observer,theta,phi,L,K,a_const,G,flap_deflection)
 
-pe_2_slat = get_effective_pressure_slat(rho_0,a_sl,M,r_observer,theta,phi,L,K,a_const,G)
 
-pe_2_wing = get_effective_pressure_wing(rho_0,a_sl,M,r_observer,theta,phi,L,K,a_const,G)
+r_observer=120
+theta_observer=radians(90)
+phi_observer=radians(40)
 
-pe_2_lg_main =  get_effective_pressure_lg(rho_0,a_sl,M,r_observer,theta,phi,d,K,N_w,d_w)
 
-pe_2_lg_noise =  get_effective_pressure_lg(rho_0,a_sl,M,r_observer,theta,phi,d,K,N_w,d_w)
 
-pe_2_strut= get_effective_pressure_strut(rho_0,a_sl,M,r_observer,theta,phi,d,K,d_strut,L_strut)
+
+pe_2_flap =[get_effective_pressure_flap(f,rho_0,a_sl,M_TO,r_observer,theta_observer,phi_observer,L_flap,K_flap,a_flap,G_flap,flap_deflection) for f in centrefreq]
+
+
+pe_2_slat =[ get_effective_pressure_slat(f,rho_0,a_sl,M_TO,r_observer,theta_observer,phi_observer,L_slat,K_slat,a_slat,G_slat)for f in centrefreq]
+
+pe_2_wing = [get_effective_pressure_wing(f,rho_0,a_sl,M_TO,r_observer,theta_observer,phi_observer,L_wing,K_wing,a_wing,G_wing)for f in centrefreq]
+
+pe_2_mlg =  [get_effective_pressure_lg(f,rho_0,a_sl,M_TO,r_observer,theta_observer,phi_observer,K_mlg,d_mw,a_lg,G_mlg)for f in centrefreq]
+
+pe_2_nlg =  [get_effective_pressure_lg(f,rho_0,a_sl,M_TO,r_observer,theta_observer,phi_observer,K_nlg,d_nw,a_lg,G_nlg)for f in centrefreq]
+
+pe_2_strut_main= [get_effective_pressure_strut(f,rho_0,a_sl,M_TO,r_observer,theta_observer,phi_observer,K_strut,L_strut_main,a_lg,G_mlg)for f in centrefreq]
+pe_2_strut_nose= [get_effective_pressure_strut(f,rho_0,a_sl,M_TO,r_observer,theta_observer,phi_observer,K_strut,L_strut_nose,a_lg,G_nlg)for f in centrefreq]
+
+
+OSPL_dBA_flap=get_overall_sound_level_general(pe_2_flap,freq_delta,centrefreq)
+OSPL_dBA_slat=get_overall_sound_level_general(pe_2_slat,freq_delta,centrefreq)
+OSPL_dBA_wing=get_overall_sound_level_general(pe_2_wing,freq_delta,centrefreq)
+OSPL_dBA_mlg=get_overall_sound_level_general(pe_2_mlg,freq_delta,centrefreq)
+OSPL_dBA_nlg=get_overall_sound_level_general(pe_2_nlg,freq_delta,centrefreq)
+
+if phi==0:
+    OSPL_dBA_mlg_strut=0
+    OSPL_dBA_nlg_strut=0
+else:
+    OSPL_dBA_mlg_strut=get_overall_sound_level_general(pe_2_strut_main,freq_delta,centrefreq)
+    OSPL_dBA_nlg_strut=get_overall_sound_level_general(pe_2_strut_nose,freq_delta,centrefreq)
+
+
+flap_fig=plot_pbl_vs_freq(pe_2_flap,centrefreq,freq_delta)
+slat_fig=plot_pbl_vs_freq(pe_2_slat,centrefreq,freq_delta)
+wing_fig=plot_pbl_vs_freq(pe_2_wing,centrefreq,freq_delta)
+mlg_fig=plot_pbl_vs_freq(pe_2_mlg,centrefreq,freq_delta)
+nlg_fig=plot_pbl_vs_freq(pe_2_nlg,centrefreq,freq_delta)
+
+
+pe_tot=[pe_2_flap [i]+pe_2_slat[i] + pe_2_wing[i] +pe_2_mlg[i]+pe_2_nlg[i] +pe_2_strut_main[i]+ pe_2_strut_nose[i]for i in range(len(centrefreq))] 
+
+OSPL_dBA_tot=get_overall_sound_level_general(pe_tot,freq_delta,centrefreq)
+
+
+
+print('flap',OSPL_dBA_flap, 'dBA' )
+print('slat',OSPL_dBA_slat, 'dBA'  )
+print('wing',OSPL_dBA_wing, 'dBA'  )
+print('mlg',OSPL_dBA_mlg , 'dBA' )
+print('nlg',OSPL_dBA_nlg , 'dBA' )
+print('m strut',OSPL_dBA_mlg_strut, 'dBA'  )
+print('n strut',OSPL_dBA_nlg_strut, 'dBA' )
+
+print('overall dBA',OSPL_dBA_tot, 'dBA' )
--- modules/sustainability/noise_defs.py
+++ modules/sustainability/noise_defs.py
@@ -5,6 +5,9 @@ Created on Tue Jun  4 11:53:00 2019
 @author: Lisa
 """
 from math import *
+from inputs.concept_1 import *
+from inputs.constants import mu_sl, rho_0,a_sl
+import matplotlib.pyplot as plt
 #noise definitions
 
 
@@ -12,22 +15,24 @@ from math import *
 #f will be a list which will be an 1./third octave band or complete octave band 
 
 'strouhal numbers'
-def get_strouhal_number(L,M,theta,a):
+def get_strouhal_number(f,L,M,theta,a):
     S=f*L*(1-M*cos(theta))/(M*a)
     return S
-
-def get_strouhal_number_lg_and_strut(d,M,a):
-    S=f*d/(M*a) # 
+def get_strouhal_number_lg_and_strut(f,d_w,M,a):
+    S=f*d_w/(M*a) # 
     return S
+
+
 'get acoustic powers'
-def get_noise_acoustic_power(K,M,a_const,G,rho,a,b):
+def get_noise_acoustic_power(K,M,a_const,G,rho,a):
     P=K*M**a_const*G*(rho*a**3*b**2)
     return P
 def get_noise_acoustic_power_landinggear(K,M,N_w,d_w):
     return K*M**6*N_w*d_w**2
 
-def get_noise_acoustic_power_strut(K,M,d_strut,L_strut):
-    return K*M**6*d_strut*L_strut
+def get_noise_acoustic_power_strut(K,M,d_w,L_strut):
+    return K*M**6*d_w*L_strut
+
 
 'spectrical functions'
 def get_spectrical_function_wing(S):
@@ -41,22 +46,25 @@ def get_spectrical_function_flap(S):
     else:
         F=216.49*S**-3
     return F
-
 def get_spectrical_function_slat(S):
     F=0.613*(10*S)**4*((10*S)**1.5+0.5)**-4 + 0.613*(2.19*S)**4*((2.19*S)**1.5+0.5)**-4
     return F
-
 def get_spectrical_function_landinggear(S): #this is for two wheels
     F=2*(13.59*S**2*(S**2+12.5)**-2.25)
     return F
+
+
+
+
+
 
 'get directivity functions'
 def get_directivity_function_wing(theta,phi):  #same for slat
     D=4*(cos(phi))**2*(cos(theta/2))**2
     return D
 
-def get_directivity_function_flap(flap_deflection_rad,theta,phi):
-    D=3*(sin(flap_deflection_rad)*cos(theta)+cos(flap_deflection_rad)*sin(theta)*cos(phi))**2
+def get_directivity_function_flap(flap_deflection,theta,phi):
+    D=3*(sin(flap_deflection)*cos(theta)+cos(flap_deflection)*sin(theta)*cos(phi))**2
     return D
 
 def get_directivity_function_landinggear(theta):
@@ -66,52 +74,58 @@ def get_directivity_function_landinggear(theta):
 def get_directivity_function_strut(theta,phi):
     D=3*(sin(theta))**2*(sin(phi))**2
     return D
+
+
+
+
+
+
 'this is to get the effective pressure level (squared)'
 
-def get_effective_pressure_flap(rho,a,M,r_observer,theta,phi,L,K,a_const,G,flap_deflection):
-    Strouhal=get_strouhal_number(L,M,theta,a)
+def get_effective_pressure_flap(f,rho,a,M,r_observer,theta,phi,L,K,a_const,G,flap_deflection):
+    Strouhal=get_strouhal_number(f,L,M,theta,a)
     F=get_spectrical_function_flap(Strouhal)
-    P=get_noise_acoustic_power(K,M,a_const,G,rho,a,b)
-    D=get_directivity_function_flap(flap_deflection_rad,theta,phi)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a)
+    D=get_directivity_function_flap(flap_deflection,theta,phi)
     
     p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
     return p_e_squared
 
-def get_effective_pressure_slat(rho,a,M,r_observer,theta,phi,L,K,a_const,G):
-    Strouhal=get_strouhal_number(L,M,theta,a)
+def get_effective_pressure_slat(f,rho,a,M,r_observer,theta,phi,L,K,a_const,G):
+    Strouhal=get_strouhal_number(f,L,M,theta,a)
     F=get_spectrical_function_slat(Strouhal)
-    P=get_noise_acoustic_power(K,M,a_const,G,rho,a,b)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a)
     D=get_directivity_function_wing(theta,phi)
     
     p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
     return p_e_squared
 
-def get_effective_pressure_wing(rho,a,M,r_observer,theta,phi,L,K,a_const,G):
-    Strouhal=get_strouhal_number(L,M,theta,a)
+def get_effective_pressure_wing(f,rho,a,M,r_observer,theta,phi,L,K,a_const,G):
+    Strouhal=get_strouhal_number(f,L,M,theta,a)
     F=get_spectrical_function_wing(Strouhal)
-    P=get_noise_acoustic_power(K,M,a_const,G,rho,a,b)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a)
     D=get_directivity_function_wing(theta,phi)
     
     p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
     return p_e_squared
 
-def get_effective_pressure_lg(rho,a,M,r_observer,theta,phi,d,K,N_w,d_w):
-    Strouhal=get_strouhal_number_lg_and_strut(d,M,a)
+def get_effective_pressure_lg(f,rho,a,M,r_observer,theta,phi,K,d_w,a_const,G):
+    Strouhal=get_strouhal_number(f,d_w,M,theta,a)
     F=get_spectrical_function_landinggear(Strouhal)
-    P=get_noise_acoustic_power_landinggear(K,M,N_w,d_w)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a)
     D=get_directivity_function_landinggear(theta)
     
     p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
-    return pe_e_squared
+    return p_e_squared
 
-def get_effective_pressure_strut(rho,a,M,r_observer,theta,phi,d,K,d_strut,L_strut):
-    Strouhal=get_strouhal_number_lg_and_strut(d,M,a)
+def get_effective_pressure_strut(f,rho,a,M,r_observer,theta,phi,K,L_strut,a_const,G):
+    Strouhal=get_strouhal_number(f,L_strut ,M,theta,a)
     F=get_spectrical_function_landinggear(Strouhal)
-    P=get_noise_acoustic_power_strut(K,M,d_strut,L_strut)
+    P=get_noise_acoustic_power(K,M,a_const,G,rho,a)
     D=get_directivity_function_strut(theta,phi)
     
     p_e_squared=rho*a*P*D*F/(4*pi*r_observer**2*(1-M*cos(theta))**4) #(pa^2)
-    return pe_e_squared
+    return p_e_squared
 
 
 
@@ -119,20 +133,64 @@ def get_effective_pressure_strut(rho,a,M,r_observer,theta,phi,d,K,d_strut,L_stru
 def get_sound_pressure_level(p_e_squared):   #[dB]
     SPL=10*log10(p_e_squared/(2E-5)**2)
     return SPL
+
 'correct for the third octave bands'
 def correction_for_thirdbandwidth(SPL,freq_delta):
     SPL_bandwidth=SPL+10*log10(freq_delta)
     return SPL_bandwidth
 
+
+
+
+
 'perform A weightening '  
-def a_weighting_correction(f):
-    delta_L_A=-145.528+98.262*log10(f)-19.509*(log10(f))**2+0.975*(log(f))**3
+def A_weighting_correction(f):
+    delta_L_A=-145.528+98.262*log10(f)-19.509*(log10(f))**2+0.975*(log10(f))**3
     return delta_L_A
  
-def A_weighted_sound_level(delta_L_A,SPL):
-    L_A_perfreq=SPL+delta_L_A
-    OSPL_A=10*log10(sum(10**(L_A_perfreq/10)))
+
+def A_weighted_sound_level(f,delta_L_A,SPL):
+    L_A_perfreq=SPL+delta_L_A 
+    return L_A_perfreq
+
+def A_weighted_overall_sound_level(centrefreq,L_A_perfreq):
+    exponent=[10**(L_A_perfreq[i]/10) for i in range(len(centrefreq))]
+    sum_exponent=sum(exponent)
+    OSPL_A=10*log10(sum_exponent)
+
     return OSPL_A
+
+
+'perform all in one'
+def get_overall_sound_level_general(p_e_squared,freq_delta,centrefreq):
+    SPL         =[get_sound_pressure_level(p_e_squared[i]) for i in range(len(centrefreq))]
+    PBL         =[SPL[i]+10*log10(freq_delta[i]) for i in range(len(centrefreq))]
+    delta_L_A   = [A_weighting_correction(centrefreq[i]) for i in range(len(centrefreq))]
+    PBL_a = [A_weighted_sound_level(centrefreq,delta_L_A[i],PBL[i])for i in range(len(centrefreq))]
+    OSPL_A      =A_weighted_overall_sound_level(centrefreq,PBL_a)
+    return OSPL_A
+
+
+
+def plot_pbl_vs_freq(p_e_squared,centrefreq,freq_delta):
+    SPL         =[get_sound_pressure_level(p_e_squared[i]) for i in range(len(centrefreq))]
+    PBL         =[SPL[i]+10*log10(freq_delta[i]) for i in range(len(centrefreq))]
+    delta_L_A   = [A_weighting_correction(centrefreq[i]) for i in range(len(centrefreq))]
+    PBL_a = [A_weighted_sound_level(centrefreq,delta_L_A[i],PBL[i])for i in range(len(centrefreq))]
+    print(delta_L_A)
+    
+    
+
+    plt.figure()
+    plt.plot(centrefreq,PBL, '-o',label='Unweighted PBL')
+    plt.plot(centrefreq,PBL_a,'-o',label='A-weighted PBL')
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('1/3 octave PBL [dB(A)]')
+    plt.xscale('log')
+    plt.xlim(10,10**4)
+    plt.title('1/3 octave Pressure Band Level A-weightening comparison')
+    plt.legend()
+    plt.show()
 
 
 'redundant definitions for now '
@@ -146,13 +204,10 @@ def get_perceived_noise_level(N):
 
 def get_effective_perceived_noise_level(L_pn,delta_t):
     L_epn=10*log10(delta_t/10*sum(10**(L_pn/10)))
-       
-    
-    
-    
-
-def get_theta_observer(x,R):
+          
+def get_theta_observer(r,h):
     return 'ok'
 
-def get_phi_observer(x,R):
+def get_phi_observer(r,h):
+    phi=np.theta(r/h)
     return 'ok'
