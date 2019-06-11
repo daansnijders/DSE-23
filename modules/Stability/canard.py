a4a370e14a0b11d31a3241de218b1b695a119322
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-06-11 11:03:11 +0200

    eom solving canard

--- modules/Stability/canard.py
+++ modules/Stability/canard.py
@@ -29,18 +29,34 @@ class canard():
         
         self.L_canard = self.additional_weight
         
-        cg = [config1_cg.calc_x_cg(), config2_cg.calc_x_cg(), config3_cg.calc_x_cg()] # [m] x-location of the c.g.
-        moment_arm = cg[self.config] - cg[0]                                    # [m] distance between canard ac and c.g.
+        # determine location by use of the moment caused by the aditional module
+        cg_x = [config1_cg.calc_x_cg(), config2_cg.calc_x_cg(), config3_cg.calc_x_cg()] # [m] x-location of the c.g.
+        cg_z = [config1_cg.calc_z_cg(), config2_cg.calc_z_cg(), config3_cg.calc_z_cg()] # [m] x-location of the c.g.
+        moment_arm = cg_x[self.config] - cg_x[0]                                # [m] distance between canard ac and c.g.
         
         moment = moment_arm * self.weight                                       # [N*m] moment caused by the c.g. shift
         
         self.l_c = moment / self.L_canard                                       # [m] required distance between canard and c.g.
-        self.x_c = cg[self.config] - self.l_c                                   # [m] x-location of canard ac
-        
-        # determine location by use of the moment caused by the aditional module
+        self.x_c = cg_x[self.config] - self.l_c                                 # [m] x-location of canard ac
         
         
+        x_c = 5                                                                 # [m] x-location of canard ac
+        l_c = cg_x[self.config] - x_c
+        l_h = x_le_h[self.config]
+        F_h = 0
+        l_cg = (x_le_MAC[self.config] + 0.25*MAC) - cg_x[self.config]
+        z_e = cg_z[self.config] - z_engine
+        F_e = thrust_max
         
+        
+        F_w = (l_c * (self.weight + F_h) + l_h * F_h + F_e * z_e) / (l_cg + l_c)
+        F_c = -F_w + self.weight + F_h
+
+        
+        margin = 1E-8
+        print(F_c, F_w, F_h)
+        assert -margin <= F_c + F_w - self.weight - F_h <= margin
+        assert -margin <= l_c * F_c - l_cg * F_w + l_h * F_h + F_e * z_e <= margin
         
         # determine aspect ratio/ taper ratio/ sweep/ ect.
         
