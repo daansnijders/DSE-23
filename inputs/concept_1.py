10c96aa8707ad61c7eb3de6099a5f24f5cca38b7
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-06-11 09:04:16 +0200

    some units added

--- inputs/concept_1.py
+++ inputs/concept_1.py
@@ -103,6 +103,7 @@ y_MAC = get_y_MAC(b, Cr, MAC, Ct)                                               
 dihedral_rad = get_dihedral_rad(lambda_4_rad)                                   # [rad] dihedral angle of the main wing
 lambda_le_rad = get_lambda_le_rad(lambda_4_rad, Cr, b, taper_ratio)             # [rad] leading edge sweep angle main wing
 
+
 #canard parameters
 A_c = 6  
 b_c = [get_b(A_c,S_c[i]) for i in range(3)]
@@ -126,6 +127,8 @@ M_fuselage, x_cg_fuselage=get_mass_fuselage(MTOW,l_f)
 M_tail,x_cg_tail=get_mass_tail(MTOW,l_f)
 M_fuselage_group, x_cg_fuselage_group=get_mass_fuselagegroup(M_fuselage,M_tail,x_cg_fuselage,x_cg_tail)
 x_le_MAC=get_x_le_MAC(l_f,MAC,M_wing_group, M_fuselage_group)
+
+x_le_w = get_le_wing(y_MAC,x_le_MAC, lambda_2_rad, MAC, Cr)
 
 x_cg_wing,x_cg_eng,x_cg_wing_group=get_cg_winggroup(x_le_MAC, MAC,M_wing, M_eng, M_wing_group )
 
--- modules/Stability/canard.py
+++ modules/Stability/canard.py
@@ -6,29 +6,38 @@ Created on Fri Jun  7 10:13:41 2019
 """
 
 from modules.Stability.Stability_runfile import *
+from modules.EXECUTE_FILE import * 
 
-class canard:
+class canard():
     def __init__ (self, weight_pass, config):
-
-        self.weight_pass = weight_pass                                          # [kg] mass increase per passenger
-        self.additional_mass = weight_pass[config][-1] - weight_pass[0][-1]     # [kg] mass difference between config 1 and 2/3
-        self.additional_weight = self.additional_mass * g                       # [N] weight difference between config 1 and 2/3
         self.config = config                                                    # [-] configuration selection
+        self.weight_pass = weight_pass                                          # [kg] mass increase per passenger
+        self.additional_mass = weight_pass[self.config][-1] - weight_pass[0][-1] # [kg] mass difference between config 1 and 2/3
+        self.additional_weight = self.additional_mass * g                       # [N] weight difference between config 1 and 2/3
+        self.weight = self.weight_pass[self.config][-1] * g                     # [N] MTOW config
+        
       
-
+        config1_cg.calc_x_cg()
+        self.size_canard()
         
     def size_canard(self):
         # determine airfoil/angle of attack during cruise
         
-        C_l_canard = 0.5
+        C_L_canard = 0.5
         # determine force required/surface area
         S_c = self.additional_weight / (C_L_canard * 0.5 * rho * V_cruise**2)   # [m^2] surface area required by canard
         
+        self.L_canard = self.additional_weight
         
+        cg = [config1_cg.calc_x_cg(), config2_cg.calc_x_cg(), config3_cg.calc_x_cg()] # [m] x-location of the c.g.
+        moment_arm = cg[self.config] - cg[0]                                    # [m] distance between canard ac and c.g.
         
+        moment = moment_arm * self.weight                                       # [N*m] moment caused by the c.g. shift
+        
+        self.l_c = moment / self.L_canard                                       # [m] required distance between canard and c.g.
+        self.x_c = cg[self.config] - self.l_c                                   # [m] x-location of canard ac
         
         # determine location by use of the moment caused by the aditional module
-        
         
         
         
@@ -36,4 +45,5 @@ class canard:
         # determine aspect ratio/ taper ratio/ sweep/ ect.
         
         
-    
+c = canard(weight_pass,2)
+
