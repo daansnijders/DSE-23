94c871e56c4e259fc3f1db0ebe6513e1515bc734
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-06-11 11:19:40 +0200

    Changed sign convention of F_h

--- modules/Stability/canard.py
+++ modules/Stability/canard.py
@@ -43,27 +43,27 @@ class canard():
         x_c = 5                                                                 # [m] x-location of canard ac
         l_c = cg_x[self.config] - x_c
         l_h = x_le_h[self.config] - cg_x[self.config]
-        F_h = 0
         l_cg = (x_le_MAC[self.config] + 0.25*MAC) - cg_x[self.config]
         z_e = cg_z[self.config] - z_engine
         F_e = thrust_max
-        
-        
-        F_w = (l_c * (self.weight + F_h) + l_h * F_h + F_e * z_e) / (l_cg + l_c)
-        F_c = -F_w + self.weight + F_h
-
-        
-        margin = 1E-8
-        print(F_c, F_w, F_h)
-        assert -margin <= F_c + F_w - self.weight - F_h <= margin
-        assert -margin <= l_c * F_c - l_cg * F_w + l_h * F_h + F_e * z_e <= margin
-
-        # determine location by use of the moment caused by the aditional module
-    
+        print(x_le_h)
         w = self.weight_pass[0][-1] * g
         x_h = 0.9* l_f[0]
         F_h = (-w*(config1_cg.x_cg_wing - config1_cg_x) + thrust_max * (config1_cg_z - config1_cg.z_cg_engines))/-(-config1_cg.x_cg_wing + config1_cg_x + x_h - config1_cg_x)
         F_w = w + F_h 
+        
+        F_w = (l_c * (self.weight - F_h) - l_h * F_h + F_e * z_e) / (l_cg + l_c)
+        F_c = -F_w + self.weight - F_h
+
+        
+        margin = 1E-8
+        print(F_c, F_w, F_h)
+        assert -margin <= F_c + F_w - self.weight + F_h <= margin
+        assert -margin <= l_c * F_c - l_cg * F_w - l_h * F_h + F_e * z_e <= margin
+
+        # determine location by use of the moment caused by the aditional module
+    
+
 
         
         # determine aspect ratio/ taper ratio/ sweep/ ect.
