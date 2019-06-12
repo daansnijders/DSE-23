7a47bc9ee5fe838766a9f8a3c00455503740d6ad
Stijn van Selling <s.vanselling@student.tudelft.nl>
2019-06-12 17:00:59 +0200

    added cg_weight_config2

--- modules/Stability/main_stability.py
+++ modules/Stability/main_stability.py
@@ -20,11 +20,3 @@ from modules.Stability.cg_weight_config1 import *
 
 e2 = empennage(1, (11.78+0.25*3.8), 3.82, 4.90, 0.3835, 21.72, 16., 93.5, 3.8, 1., 11.78, -0.3, 1.6, x_cg_max, -0.5838, )
 
-
-
-#c2 = canard(weight_pass,2, 1.3)        
-#c3 = canard(weight_pass,3, 1.3)
-#
-#print('Canard: ' + str(c3.F_c / c2.F_c *100 - 100))
-#print('Main w: ' + str(c3.F_w / c2.F_w *100 - 100))
-#print('H tail: ' + str(c3.F_h / c2.F_h *100 - 100))
