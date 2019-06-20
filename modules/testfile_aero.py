85e672eeb8fe0c346e323cf8d1e0312d5348fc6c
SybrenBootsma <47575390+SybrenBootsma@users.noreply.github.com>
2019-06-20 13:56:34 +0200

    update CL-alpha plot

--- modules/testfile_aero.py
+++ modules/testfile_aero.py
@@ -147,7 +147,7 @@ delta_CL3, delta_CL_alpha3, delta_CL_max3, delta_CL_TO3, delta_CL_alpha_TO3, del
 #print(CL_alpha_h3, CL_alpha_c3, CL_alpha3, alpha_0_L3, CL_max3)
 
 
-plot = config1_Lift.CL_alpha_plot(CL_alpha1, alpha_0_L1, CL_max1, alpha_CL_max1, delta_CL1, delta_CL_alpha1, delta_CL_max1, delta_CL_TO1, delta_CL_alpha_TO1, delta_CL_max_TO1)
+plot = config1_Lift.CL_alpha_plot(CL_alpha1, alpha_0_L1, CL_max1, alpha_CL_max1, delta_CL1, delta_CL_alpha1, delta_CL_max1)
 
 
 """ Moment classII estimations """
@@ -200,5 +200,5 @@ delta_Cm_w_flaps3, delta_Cm_w_krueger3 = config3_Moment.Wing_moment_flaps(Cm0_w_
 #CD_land4 = CD_w_sub4 + CD_fus_sub4 + CD_h_sub4 + CD_v_sub4 + CD_c_sub4 + CD_flap_land4 + CD_slat4 + CD_gear4 + CD_ws4 + CD_store_sub4 + CD_trim4
 
 
-C_L, C_L_flaps = config1_Lift.get_CL(CL_alpha1, alpha_0_L1, CL_max1, alpha_CL_max1, delta_CL1, delta_CL_alpha1, delta_CL_max1, 0.58)
+C_L, C_L_flaps = config1_Lift.get_CL(CL_alpha1, alpha_0_L1, CL_max1, alpha_CL_max1, delta_CL1, delta_CL_alpha1, delta_CL_max1, 4)
 #print (C_L, C_L_flaps)
